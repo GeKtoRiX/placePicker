2381d7f94e9510ba157318fafe1df1cb85de9c67
GeKtoRiX <gektor93@gmail.com>
2025-11-12 03:04:13 +0300

    small fixes/finished vite template

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
@@ -70,23 +70,6 @@ export default defineConfig(({ command }) => {
       chunkSizeWarningLimit: 1000,
       minify: isBuild ? 'esbuild' : false,
       cssMinify: isBuild,
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (!id.includes('node_modules')) return;
-            const normalized = id.replace(/\\/g, '/');
-            const [, afterNodeModules] = normalized.split('node_modules/');
-            if (!afterNodeModules) return;
-            const parts = afterNodeModules.split('/');
-            if (!parts.length) return;
-            const pkg =
-              parts[0].startsWith('@') && parts.length > 1
-                ? `${parts[0]}/${parts[1]}`
-                : parts[0];
-            return `vendor-${pkg.replace('@', '').replace('/', '-')}`;
-          },
-        },
-      },
     },
     esbuild: {
       legalComments: 'none',
